ae78e7264d3a09580f3bde4d5728c711c1dfd410
petersylvain95-create <petersylvain95@gmail.com>
2025-10-31 19:58:29 -0400

    Fix typo in variable name for fs module

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+if (s.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
